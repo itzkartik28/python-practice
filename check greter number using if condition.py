496bf0279da66195ffcbfc4e54dd1e21a8af2017
kartik salunkhe <kartiksalunkhe2801@gmail.com>
2026-08-02 22:59:35 +0530

    check the greater number

---
+++ check greter number using if condition.py
@@ -0,0 +1,14 @@
+a=int(input("enter number 1:"))
+a1=int(input("enter number 2:"))
+a2=int(input("enter number 3:"))
+a3=int(input("enter number 4:"))
+
+if(a>a1) and (a>a2):
+    print("number 1 is grater",a)
+elif(a1>a2) and (a1>a3):
+    print("number 2 is grater",a1)
+elif(a2>a1)and(a2>a3):
+    print("number 3 is greter")
+
+else:
+    print("number 4 is greter")
