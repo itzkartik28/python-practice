f0560cc89aac0381acf876b2c2a01a3b4bedab83
kartik salunkhe <kartiksalunkhe2801@gmail.com>
2026-08-03 23:50:04 +0530

    Add files via upload

---
+++ dist method.py
@@ -0,0 +1,17 @@
+marks={
+
+    "shivam":50,
+    "pavan":51,
+    "kartik": 95
+}
+
+#print(marks.items())
+
+#print(marks.keys())
+
+#print(marks.values())
+
+#marks.update({"kartik":99 ,"parth":98})
+#print(marks)
+
+print(marks.get("kartik"))
