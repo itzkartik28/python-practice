29e69d35a1a58ef6ba1ff264c9df2799b893e47d
kartik salunkhe <kartiksalunkhe2801@gmail.com>
2026-08-03 23:51:28 +0530

    Add files via upload

---
+++ dist user.py
@@ -0,0 +1,23 @@
+d={}
+
+name=input("enter a name:")
+lang=input("enter a lang:")
+d.update({name:lang})
+
+name=input("enter a name:")
+lang=input("enter a lang:")
+d.update({name:lang})
+
+name=input("enter a name:")
+lang=input("enter a lang:")
+d.update({name:lang})
+
+name=input("enter a name:")
+lang=input("enter a lang:")
+d.update({name:lang})
+
+name=input("enter a name:")
+lang=input("enter a lang:")
+d.update({name:lang})
+
+print(d)
