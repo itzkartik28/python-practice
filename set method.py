b32a31041eb4863f9dbc96fd1dab0f014406ed04
kartik salunkhe <kartiksalunkhe2801@gmail.com>
2026-08-07 22:27:31 +0530

    Add files via upload

---
+++ set method.py
@@ -0,0 +1,8 @@
+s={1,44,88,9,9,"kartik"}
+
+print(s,type(s))
+
+s.add(555)
+print(s,type(s))
+s.remove(1)
+print(s)
---
+++ set union intersection.py
@@ -0,0 +1,5 @@
+s1={1,3,4,66}
+s2={1,3,44,55,6,7,7}
+
+print(s1.union(s2))
+print(s1.intersection(s2))
